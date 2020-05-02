956170d24ee610200d315e293b15cae90bafba61
lucky624 <lironnanda@gmail.com>
2020-05-02 21:02:23 +0600

    delete old pcaps

--- extractor.py
+++ extractor.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-import os, re
+import os, re, time
 
 
 pwd_files = []
@@ -8,20 +8,28 @@ for d, dirs, files in os.walk('data'):
     pwd_files = files
     break
 
-GO = 1
+for file in pwd_files:
+    f = open(d + '/' + file, 'rb')
+    for line in f.readlines():
+        try:
+            decode_line = line.decode()
+            flags = re.findall("[A-Z0-9]{31}=", decode_line)
+            if flags:
+                for flag in flags:
+                    print(flag, flush=True)
+        except:
+            continue
+    f.close()
 
-if GO != 1:
-    print('stop')
-else:
-    for file in pwd_files:
-        f = open(d + '/' + file, 'rb')
-        for line in f.readlines():
-            try:
-                decode_line = line.decode()
-                flags = re.findall("[A-Z0-9]{31}=", decode_line)
-                if flags:
-                    for flag in flags:
-                        print(flag, flush=True)
-            except:
-                continue
-        f.close()
+current_time = time.time()
+pcaps = os.listdir('data/')
+
+for pcap in pcaps:
+    if pcap == 'record.sh' or pcap == 'config':
+        continue
+    if (current_time - os.path.getctime('data/' + pcap))//60 > 5:
+        try:
+            os.remove('data/'+pcap)
+            print(pcap + ' was deleted. He is older then 5 minutes.')
+        except IsADirectoryError:
+            continue
--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-./start_sploit.py extractor.py --server-url http://92.124.129.15 --not-per-team --pool-size 1 --attack-period 5
+./start_sploit.py extractor.py --server-url http://92.124.129.15 --not-per-team --pool-size 1 --attack-period 30
